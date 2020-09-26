4c20ac6b5b442e13555faebad2982ec266cdf97a
MinhGreen <71547303+MinhGreen@users.noreply.github.com>
2020-09-26 23:00:26 +0700

    Add files via upload

---
+++ B2.py
@@ -0,0 +1,7 @@
+sum =0
+n =int(input("Nhập N: "))
+print("Từ 1 đến n: ",end='')
+for i in range (1,n+1,1):
+    sum = sum +i
+    print(i,end=' ')
+print("= "+str(sum))
---
+++ B6.py
@@ -0,0 +1,9 @@
+sum=0
+n =int(input("Nhập N: "))
+print("Từ 1 đến n: ",end='')
+for i in range (1,n+1,1):
+    if i==13:
+        break
+    sum = sum +i
+    print(i,end=' ')
+print("= "+str(sum))
